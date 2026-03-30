1746f08ff092cae38ab82af48d2a9819ff7f5d31
BnnadiTUD <C22469126@mytudublin.ie>
2026-03-30 20:24:06 +0100

    Created basic admin and product entity

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,28 @@
+package model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Admin extends PanacheEntity {
+
+    public String name;
+
+    public String email;
+
+    public String passwordHash;
+
+    public String role = "ADMIN";
+
+
+    public Admin() {
+    }
+
+    public Admin(String name, String email, String passwordHash) {
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = "ADMIN";
+    }
+}
---
+++ src/main/java/model/Item.java
@@ -0,0 +1,35 @@
+package model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+
+@Entity
+public class Item extends PanacheEntity {
+
+    public String title;
+
+    public String manufacturer;
+
+    public String category;
+
+    public double price;
+
+    public String description;
+
+    public int stockQuantity;
+
+    // empty constructor 
+    public Item() {}
+
+    // Constructor for easy creation
+    public Item(String title, String manufacturer, String category,
+                   double price, String description, int stockQuantity) {
+        this.title = title;
+        this.manufacturer = manufacturer;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+    }
+}
